93f7eea283cd7df8a4a858386edeb685db30b660
Eric Schneider <eschneider@dkssystems.com>
2021-04-24 20:28:50 -0500

    auto download database

--- sources/openvgdb.py
+++ sources/openvgdb.py
@@ -1,6 +1,11 @@
+import io
+import log
 import pathlib
-import sqlite3
 import re
+import requests
+import shutil
+import sqlite3
+import zipfile
 
 class OpenVgdbSource:
     def __init__(self, config):
@@ -8,6 +13,7 @@ class OpenVgdbSource:
         self.database_path = pathlib.Path(config['database_path'])
 
     def open(self):
+        self.ensure_database()
         self.conn = sqlite3.connect(str(self.database_path))
         self.cur = self.conn.cursor()
         self.ensure_index('ROMs', 'romHashSHA1')
@@ -23,11 +29,49 @@ class OpenVgdbSource:
         bits = map(lambda b: b.strip(), bits)
         return list(bits)
 
+    def get_database_url(self):
+        with requests.get('https://api.github.com/repos/OpenVGDB/OpenVGDB/releases/latest') as r:
+            r.raise_for_status()
+
+            release = r.json()
+            if not release:
+                raise Exception('Failed to get release data')
+
+            assets = release.get('assets', None)
+            if not assets:
+                raise Exception('Release does not contain any assets')
+            
+            assets = list(filter(lambda a: a['name'] == 'openvgdb.zip', assets))
+            if len(assets) == 0:
+                raise Exception('Release does not contain openvgdb.zip')
+
+            url = assets[0].get('browser_download_url', None)
+            if not url:
+                raise Exception('Release does not contain download url')
+
+            return url
+
+    def ensure_database(self):
+        if self.database_path.is_file():
+            return
+
+        log.info('Downloading OpenVGDB')
+
+        url = self.get_database_url()
+        with requests.get(url) as r:
+            r.raise_for_status()
+            with io.BytesIO() as mem:
+                mem.write(r.content)
+                with zipfile.ZipFile(mem) as zip:
+                    with zip.open('openvgdb.sqlite') as zf, open(str(self.database_path), 'wb') as f:
+                        shutil.copyfileobj(zf, f)
+
     def ensure_index(self, table, field):
         index_name = 'idx_' + table + '_' + field
         self.cur.execute("PRAGMA index_list('" + table + "')")
         indexes = [row[1] for row in self.cur.fetchall()]
         if index_name not in indexes:
+            log.info('Creating database index {}', index_name)
             self.cur.execute("CREATE INDEX " + index_name + " ON " + table + " (" + field + ")")
     
     def rom_data(self, rom):
